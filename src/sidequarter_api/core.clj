1d058cd9db3e041721a1232494a7695006e84c8a
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-20 22:09:03 +0200

    Add basic hello world api

--- src/sidequarter_api/core.clj
+++ src/sidequarter_api/core.clj
@@ -1,6 +1,15 @@
-(ns sidequarter-api.core)
+(ns sidequarter-api.core
+  (:require [liberator.core :refer [resource defresource]]
+            [ring.middleware.params :refer [wrap-params]]
+            [compojure.core :refer [defroutes GET]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes app
+  (GET "/" [] index-action))
+
+(def handler
+  (-> app
+      wrap-params))
+
+(defresource index-action
+  :available-media-types ["application/json"]
+  :handle-ok { :message "Hello, World!" })
