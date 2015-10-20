f94f7f4df6689a08f37fefd98864eacc0ffa6eaa
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-20 22:07:58 +0200

    Add liberator, compojure & ring to dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject sidequarter-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+(defproject sidequarter-api "0.0.0-ALPHA"
+  :description "An API backend to monitor multiple Sidekiq instances"
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler sidequarter-api.core/handler}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [liberator "0.13"]
+                 [compojure "1.4.0"]
+                 [ring/ring-core "1.4.0"]])
