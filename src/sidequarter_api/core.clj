b3c8727018349c37b29d324ea64747d305c85ad3
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-20 22:12:20 +0200

    Move index-action resource to top so it can be found

--- src/sidequarter_api/core.clj
+++ src/sidequarter_api/core.clj
@@ -3,13 +3,13 @@
             [ring.middleware.params :refer [wrap-params]]
             [compojure.core :refer [defroutes GET]]))
 
+(defresource index-action
+  :available-media-types ["application/json"]
+  :handle-ok { :message "Hello, World!" })
+
 (defroutes app
   (GET "/" [] index-action))
 
 (def handler
   (-> app
       wrap-params))
-
-(defresource index-action
-  :available-media-types ["application/json"]
-  :handle-ok { :message "Hello, World!" })
