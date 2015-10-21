a39aca3b3913aed2ddd58d7da7c51d7784d4e9fd
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-21 21:49:24 +0200

    Add database migration deps to project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,16 @@
 (defproject sidequarter-api "0.0.0-ALPHA"
   :description "An API backend to monitor multiple Sidekiq instances"
   :plugins [[lein-ring "0.9.7"]
-            [lein-kibit "0.1.2"]]
+            [lein-kibit "0.1.2"]
+            [lein-environ "1.0.1"]
+            [migratus-lein "0.2.0"]]
   :ring {:handler sidequarter-api.core/handler}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [org.postgresql/postgresql "9.4-1204-jdbc42"]
                  [liberator "0.13"]
                  [compojure "1.4.0"]
-                 [ring/ring-core "1.4.0"]])
+                 [environ "1.0.1"]
+                 [migratus "0.8.7"]
+                 [ring/ring-core "1.4.0"]]
+  :migratus {:store :database})
