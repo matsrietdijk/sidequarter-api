94043549a635bee624a67415cd2916a6668974d5
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-21 22:39:18 +0200

    Show all sidekiqs in index get action

--- src/sidequarter_api/core.clj
+++ src/sidequarter_api/core.clj
@@ -1,12 +1,13 @@
 (ns sidequarter-api.core
   (:require [liberator.core :refer [resource defresource]]
             [ring.middleware.params :refer [wrap-params]]
-            [compojure.core :refer [defroutes ANY]]))
+            [compojure.core :refer [defroutes ANY]]
+            [sidequarter-api.sidekiqs :refer :all]))
 
 (defresource index-action
   :available-media-types ["application/json"]
   :allowed-methods [:get :options]
-  :handle-ok { :message "Hello, World!" })
+  :handle-ok {:sidekiqs (sidekiqs)})
 
 (defroutes app
   (ANY "/" [] index-action))
