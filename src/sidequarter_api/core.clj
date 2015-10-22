288e8a93560392583772f5f33f2b551564b8fc25
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-22 22:50:38 +0200

    Add show for sidekiqs

--- src/sidequarter_api/core.clj
+++ src/sidequarter_api/core.clj
@@ -2,15 +2,33 @@
   (:require [liberator.core :refer [resource defresource]]
             [ring.middleware.params :refer [wrap-params]]
             [compojure.core :refer [defroutes ANY]]
-            [sidequarter-api.sidekiqs :refer :all]))
+            [sidequarter-api.util :refer [->valid-id]]
+            [sidequarter-api.sidekiqs :as sidekiqs]))
 
-(defresource index-action
-  :available-media-types ["application/json"]
+(defn status-response [code message]
+  {:code code
+   :message message})
+
+(def resource-defaults
+  {:available-media-types ["application/json"]
+   :handle-not-found (status-response 404 "Not found")
+   :handle-exception (status-response 500 "Internal server error")})
+
+(defresource index-action resource-defaults
   :allowed-methods [:get :options]
-  :handle-ok {:sidekiqs (sidekiqs)})
+  :handle-ok {:sidekiqs (sidekiqs/all)})
+
+(defresource show-action [id] resource-defaults
+  :allowed-methods [:get :options]
+  :exists? (some->> (->valid-id id)
+                    sidekiqs/find-by-id
+                    (hash-map ::entry))
+  :handle-ok (fn [ctx]
+               {:sidekiq (::entry ctx)}))
 
 (defroutes app
-  (ANY "/" [] index-action))
+  (ANY "/" [] index-action)
+  (ANY "/:id" [id] (show-action id)))
 
 (def handler
   (wrap-params app))
