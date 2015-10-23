d25abc404ee83e1f232586523be72c29f6394ec6
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-23 16:29:54 +0200

    Add queues action for sidekiq instances

--- src/sidequarter_api/core.clj
+++ src/sidequarter_api/core.clj
@@ -3,20 +3,22 @@
             [ring.middleware.params :refer [wrap-params]]
             [compojure.core :refer [defroutes ANY]]
             [compojure.route :as route]
-            [sidequarter-api.util :refer [->valid-id]]
+            [sidequarter-api.util :refer [->valid-id not-found-resp
+                                          internal-error-resp]]
             [sidequarter-api.sidekiqs :as sidekiqs]))
 
-(defn status-response [code message]
-  {:code code
-   :message message})
+(defn get-entry-hash [id]
+  (some->> (->valid-id id)
+           sidekiqs/find-by-id
+           (hash-map ::entry)))
 
 (def resource-defaults
   {:available-media-types ["application/json"]
-   :handle-not-found (status-response 404 "Not found")
-   :handle-exception (status-response 500 "Internal server error")})
+   :handle-not-found not-found-resp})
+   ; :handle-exception internal-error-resp})
 
 (defresource not-found-action resource-defaults
-  :handle-ok (status-response 404 "Not found"))
+  :handle-ok not-found-resp)
 
 (defresource index-action resource-defaults
   :allowed-methods [:get :options]
@@ -24,15 +26,22 @@
 
 (defresource show-action [id] resource-defaults
   :allowed-methods [:get :options]
-  :exists? (some->> (->valid-id id)
-                    sidekiqs/find-by-id
-                    (hash-map ::entry))
+  :exists? (get-entry-hash id)
   :handle-ok (fn [ctx]
                {:sidekiq (::entry ctx)}))
+
+(defresource queues-action [sidekiq-id] resource-defaults
+  :allowed-methods [:get :options]
+  :exists? (get-entry-hash sidekiq-id)
+  :handle-ok (fn [ctx]
+               (->> (::entry ctx)
+                    (sidekiqs/queues)
+                    (hash-map :queues))))
 
 (defroutes app
   (ANY "/" [] index-action)
   (ANY "/:id" [id] (show-action id))
+  (ANY "/:sidekiq-id/queues" [sidekiq-id] (queues-action sidekiq-id))
   (route/not-found not-found-action))
 
 (def handler
