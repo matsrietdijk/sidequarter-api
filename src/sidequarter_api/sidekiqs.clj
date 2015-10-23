dd931a398a9d5c93b95245397d3e2ad6123356d2
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-23 16:28:41 +0200

    Add functions to sidekiqs helpers to get queues

--- src/sidequarter_api/sidekiqs.clj
+++ src/sidequarter_api/sidekiqs.clj
@@ -1,5 +1,8 @@
 (ns sidequarter-api.sidekiqs
   (:require [yesql.core :refer [defqueries]]
+            [clojure.string :refer [blank?]]
+            [sidequarter-api.util :refer [wcar*]]
+            [taoensso.carmine :as car :refer (smembers)]
             [environ.core :refer [env]]))
 
 (defqueries "queries/sidekiqs.sql"
@@ -7,3 +10,17 @@
 
 (defn find-by-id [id]
   (first (where-id {:id id})))
+
+(defn conn [sk]
+  {:spec {:uri (:redis_url sk)
+          :db 0}})
+
+(defn with-ns [sk key]
+  (let [namespace (:namespace sk)]
+    (if (blank? namespace)
+      key
+      (str namespace ":" key))))
+
+(defn queues [sk]
+  (wcar* (conn sk)
+         (car/smembers (with-ns sk "queues"))))
