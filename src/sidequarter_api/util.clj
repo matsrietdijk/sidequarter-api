6d3bc97866b9f3eafa6e88c62ffceac85096ccb6
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-23 16:29:33 +0200

    Move core helpers to util

--- src/sidequarter_api/util.clj
+++ src/sidequarter_api/util.clj
@@ -1,7 +1,20 @@
-(ns sidequarter-api.util)
+(ns sidequarter-api.util
+  (:require [taoensso.carmine :as car :refer (wcar)]))
 
 (defn ->valid-id [id]
   (some->> (str id)
            (re-find #"^[1-9][0-9]*$")
            read-string
            int))
+
+(defn status-response [code message]
+  {:code code
+   :message message})
+
+(defn internal-error-resp []
+  (status-response 500 "Internal server error"))
+
+(defn not-found-resp []
+  (status-response 404 "Not found"))
+
+(defmacro wcar* [conn & body] `(car/wcar ~conn ~@body))
