98974f42fb4d71768cfeb92a450f870661e1a76e
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-25 18:04:11 +0100

    Add facts about core

--- test/sidequarter_api/core_test.clj
+++ test/sidequarter_api/core_test.clj
@@ -1,7 +1,19 @@
 (ns sidequarter-api.core-test
   (:require [midje.sweet :refer :all]
+            [sidequarter-api.util :refer [not-found-resp]]
+            [sidequarter-api.sidekiqs :as sidekiqs]
             [sidequarter-api.core :refer :all]))
 
-(facts "about setting up midje"
-  (fact "it works"
-    1 => 1))
+(facts "about `get-entry-hash`"
+       (fact "it returns nil for not existing id"
+             (get-entry-hash -1) => nil
+             (get-entry-hash "a") => nil)
+       (fact "it returns an entry hash for existing id"
+             (get-entry-hash 1) =not=> nil))
+
+(facts "about `resource-defaults`"
+       (fact "the default media type is json"
+             (:available-media-types resource-defaults) => ["application/json"])
+       (fact "it provides not found handler"
+             (:handle-not-found resource-defaults) =not=> nil
+             (:handle-not-found resource-defaults) => not-found-resp))
