f059fe3e975879f08e37f31cd645f44976e1744c
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-26 17:46:36 +0100

    Add fact about get entry hash

--- test/sidequarter_api/core_test.clj
+++ test/sidequarter_api/core_test.clj
@@ -4,12 +4,16 @@
             [sidequarter-api.sidekiqs :as sidekiqs]
             [sidequarter-api.core :refer :all]))
 
+(def entry
+  (sidekiqs/find-by-id 1))
+
 (facts "about `get-entry-hash`"
        (fact "it returns nil for not existing id"
              (get-entry-hash -1) => nil
              (get-entry-hash "a") => nil)
        (fact "it returns an entry hash for existing id"
-             (get-entry-hash 1) =not=> nil))
+             (get-entry-hash 1) =not=> nil
+             (:sidequarter-api.core/entry (get-entry-hash 1)) => entry))
 
 (facts "about `resource-defaults`"
        (fact "the default media type is json"
