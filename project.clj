bf137db9499747b173057b99d76939ff54fbe8a9
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-28 23:31:26 +0100

    Add clj-time for date & time functions

--- project.clj
+++ project.clj
@@ -13,6 +13,7 @@
                  [environ "1.0.1"]
                  [migratus "0.8.7"]
                  [yesql "0.5.1"]
+                 [clj-time "0.11.0"]
                  [com.taoensso/carmine "2.12.0"]
                  [ring/ring-core "1.4.0"]
                  [ring.middleware.jsonp "0.1.6"]]
