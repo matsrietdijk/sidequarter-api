5eedd0a5b6e57ac3966dc0587578537b79b90d19
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-30 21:05:28 +0100

    Add param helpers to utils

--- src/sidequarter_api/util.clj
+++ src/sidequarter_api/util.clj
@@ -1,5 +1,8 @@
 (ns sidequarter-api.util
-  (:require [taoensso.carmine :as car :refer (wcar)]))
+  (:require [taoensso.carmine :as car :refer (wcar)]
+            [clj-time.format :as format]))
+
+(def day-formatter (format/formatter "YYYY-MM-dd"))
 
 (defn ->valid-id [id]
   (some->> (str id)
@@ -13,6 +16,22 @@
                      read-string
                      int)]
     (if (nil? res) 0 res)))
+
+(defn positive-int! [val]
+  (let [res (some->> (str val)
+                     (re-find #"^[1-9][0-9]*$")
+                     bigint
+                     int)]
+    (if (nil? res)
+      (throw (NumberFormatException. "Not parsable to a positive integer"))
+      res)))
+
+(defn date! [val]
+  (format/parse day-formatter val))
+
+(defn opt-query-param! [context name parser default]
+  (let [raw (get-in context [:request :query-params name])]
+    (if (nil? raw) default (parser raw))))
 
 (defn status-response [code message]
   {:code code
