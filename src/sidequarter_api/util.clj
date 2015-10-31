0eb7d2a536c4e168f9df6db1a2c0e392fe32bfa2
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-31 12:40:07 +0100

    Remove parsers from util ns

--- src/sidequarter_api/util.clj
+++ src/sidequarter_api/util.clj
@@ -1,33 +1,5 @@
 (ns sidequarter-api.util
-  (:require [taoensso.carmine :as car :refer (wcar)]
-            [clj-time.format :as format]))
-
-(def day-formatter (format/formatter "YYYY-MM-dd"))
-
-(defn ->valid-id [id]
-  (some->> (str id)
-           (re-find #"^[1-9][0-9]*$")
-           read-string
-           int))
-
-(defn ->int [v]
-  (let [res (some->> (str v)
-                     (re-find #"^-?[0-9]+")
-                     read-string
-                     int)]
-    (if (nil? res) 0 res)))
-
-(defn positive-int! [val]
-  (let [res (some->> (str val)
-                     (re-find #"^[1-9][0-9]*$")
-                     bigint
-                     int)]
-    (if (nil? res)
-      (throw (NumberFormatException. "Not parsable to a positive integer"))
-      res)))
-
-(defn date! [val]
-  (format/parse day-formatter val))
+  (:require [taoensso.carmine :as car :refer (wcar)]))
 
 (defn opt-query-param! [context name parser default]
   (let [raw (get-in context [:request :query-params name])]
