d1ddc8d5647100c5b702436dd4f118d2a64c857c
Mats Rietdijk <matsrietdijk@gmail.com>
2015-10-31 13:15:52 +0100

    Fix util facts

--- test/sidequarter_api/util_test.clj
+++ test/sidequarter_api/util_test.clj
@@ -2,36 +2,22 @@
   (:require [midje.sweet :refer :all]
             [sidequarter-api.util :refer :all]))
 
-(facts "about `->valid-id`"
-       (fact "it returns nil with 0 or invalid value"
-             (->valid-id -1) => nil
-             (->valid-id 0) => nil
-             (->valid-id "1a") => nil
-             (->valid-id "01") => nil
-             (->valid-id "a") => nil)
-       (fact "it returns a valid value as int"
-             (->valid-id "1") => integer?
-             (->valid-id "1") => 1
-             (->valid-id "10") => 10
-             (->valid-id "789") => 789
-             (->valid-id 1) => 1))
+(defn ^:private context []
+  {:request {:query-params {"key" "val"}}})
 
-(facts "about `->int`"
-       (fact "it returns 0 for unconvertable values"
-             (->int "a") => 0
-             (->int "a1") => 0)
-       (fact "it returns a convertable value as int"
-             (->int 1) => integer?
-             (->int 1) => 1
-             (->int "1") => 1
-             (->int "-1") => -1
-             (->int "1a") => 1
-             (->int "10") => 10
-             (->int "789") => 789)
-       (fact "it only converts until non-numerical character"
-             (->int "1,000") => 1
-             (->int "1.000") => 1
-             (->int "1;000") => 1))
+(facts "about `opt-query-param!`"
+       (fact "it returns valid available vals"
+             (opt-query-param! (context) "key" identity nil) => "val")
+       (fact "it returns default if key not available"
+             (opt-query-param! (context) "invalid" identity "default") => "default"
+             (opt-query-param! {} "invalid" identity "default") => "default")
+       (fact "it throws parser exceptions"
+             (opt-query-param! (context) "key" bigint nil) => (throws Exception)
+             (opt-query-param! (context) "key" bigint nil) => (throws NumberFormatException))
+       (fact "it does not parse default"
+             (opt-query-param! (context) "invalid" bigint "default") =not=> (throws Exception)
+             (opt-query-param! {} "invalid" bigint "default") =not=> (throws Exception)
+             (opt-query-param! {} "invalid" bigint "default") => "default"))
 
 (facts "about `status-response`"
        (fact "it has a code & a message"
